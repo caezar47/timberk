e6ffe688005454dcd3e0d64cc09522933a96b497
caezar47 <iss47@ya.ru>
2021-01-27 16:56:55 +0300

    добавил карточку каталога

--- src/block/plugin/slick/.document-ready.js
+++ src/block/plugin/slick/.document-ready.js
@@ -1,12 +1,15 @@
 var prevArrow = '<button type="button" class="slick-btn  is--prev"><span class="sr-only">Предыдущий слайд</span></button>';
 var nextArrow = '<button type="button" class="slick-btn  is--next"><span class="sr-only">Следующий слайд</span></button>';
 
-var index  		= $('[data-slick-index]');
-var snew  		= $('[data-slick-new]');
-var articles  	= $('[data-slick-articles]');
+var index  			= $('[data-slick-index]');
+var snew  			= $('[data-slick-new]');
+var articles  		= $('[data-slick-articles]');
+var articles_lg  	= $('[data-slick-articles-lg]');
+var catalog 		= $('[data-slick-catalog]');
+var catalog_nav 	= $('[data-slick-catalog-nav]');
+var catalog_note 	= $('[data-slick-catalog-note]');
 
 
-var team  		= $('[data-slick-team]');
 var team_full  	= $('[data-slick-team-full]');
 var adv 		= $('[data-slick-adv]');
 
@@ -28,7 +31,7 @@ index.slick({
 	autoplaySpeed: 7000,
 });
 snew.slick({
-	slidesToShow: 6,
+	slidesToShow: 5,
 	slidesToScroll: 1,
 	arrows: true,
 	dots: false,		
@@ -37,7 +40,6 @@ snew.slick({
 	prevArrow: prevArrow,
 	nextArrow: nextArrow,
 	responsive: [
-	   
 	    {
 			breakpoint: 1200,
 			settings: {				
@@ -79,27 +81,72 @@ articles.slick({
 	]
 });
 
-
-
-
-team.slick({
+catalog.slick({
 	slidesToShow: 1,
 	slidesToScroll: 1,
-	dots: false,
-	fade: true, 	
+	arrows: false,		
 	infinite: true, 
+	fade: true, 
+  	asNavFor: catalog_nav,
 	//autoplay: true,
 	//autoplaySpeed: 4000,
-	prevArrow: prevArrow,
-	nextArrow: nextArrow,
-  	responsive: [	
+	//prevArrow: prevArrow,
+	//nextArrow: nextArrow,
+  	/*responsive: [	
 	    {
 			breakpoint: 768,
 			settings: {
+				dots: true,
+			}
+	    }
+	]*/
+});
+catalog_nav.slick({
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	asNavFor: catalog,
+	//centerMode: true,
+	arrows: true,	
+	vertical: true,	
+	//variableWidth: true,
+	prevArrow: prevArrow,
+	nextArrow: nextArrow,
+	focusOnSelect: true
+});
+catalog_note.slick({
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	prevArrow: prevArrow,
+	nextArrow: nextArrow,
+	draggable: false,
+	pauseOnFocus: false,
+	pauseOnHover: false,
+	swipe: false,
+	touchMove: false,
+	fade: true, 	
+	autoplay: true,
+	speed: 1000,
+	autoplaySpeed: 7000,
+});
+articles_lg.slick({
+	slidesToShow: 2,
+	slidesToScroll: 1,
+	arrows: true,
+	dots: false,	
+	infinite: false,
+	prevArrow: prevArrow,
+	nextArrow: nextArrow,
+	responsive: [
+	    {
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 1,
 			}
 	    }
 	]
 });
+
+
 adv.slick({
 	slidesToShow: 1,
 	slidesToScroll: 1,
